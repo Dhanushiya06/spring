f07eb4aeec8ef73cde92994b3259acdac13d393d
dhanushiya06 <dhanushiya510@gmail.com>
2022-08-22 17:50:25 +0530

    sec

--- src/main/java/com/nseit/spring/BinarySearchImpl.java
+++ src/main/java/com/nseit/spring/BinarySearchImpl.java
@@ -1,10 +1,15 @@
 package com.nseit.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
     @Autowired
     // Autowired by name

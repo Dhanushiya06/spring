f3b0521e08666dabce27a382563bb419fa62522b
dhanushiya06 <dhanushiya510@gmail.com>
2022-08-22 17:33:14 +0530

    sec

--- src/main/java/com/nseit/spring/BinarySearchImpl.java
+++ src/main/java/com/nseit/spring/BinarySearchImpl.java
@@ -1,22 +1,24 @@
 package com.nseit.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BinarySearchImpl {
     @Autowired
+    @Qualifier("quick")
     private SortAlg sortAlg;
 
     // Constructor injection
-    public BinarySearchImpl(SortAlg sortAlg) {
-        this.sortAlg = sortAlg;
-    }
+//    public BinarySearchImpl(SortAlg sortAlg) {
+//        this.sortAlg = sortAlg;
+//    }
 
     // Setter injection
-    public void setSortAlg(SortAlg sortAlg) {
-        this.sortAlg = sortAlg;
-    }
+//    public void setSortAlg(SortAlg sortAlg) {
+//        this.sortAlg = sortAlg;
+//    }
 
     public int binarySearch(int[] numbers, int numberToSearch) {
         //  BubbleSortAlg bubbleSort = new BubbleSortAlg();
